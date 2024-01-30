c4e6e36ed1734ebb3ccc9a77f2728942ca1a0e38
hablu <131282586+hablu@users.noreply.github.com>
2024-01-30 11:22:26 +0600

    first commit

---
+++ src/app/product/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const productPage = () => {
+  return (
+    <div>
+      <h1>This is product page</h1>
+    </div>
+  );
+};
+
+export default productPage;
---
+++ src/app/profile/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const profilePage = () => {
+  return (
+    <div>
+      <h1>This is profile page</h1>
+    </div>
+  );
+};
+
+export default profilePage;
