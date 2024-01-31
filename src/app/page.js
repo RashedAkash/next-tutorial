16da099adbe7a3d44f37ffcce678e262a880d7b5
hablu <131282586+hablu@users.noreply.github.com>
2024-01-31 08:33:41 +0600

    use router added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
+"use client"
+import { useRouter } from 'next/navigation';
+
 import React from 'react';
 
 const page = () => {
+const router = useRouter()
+  const goTo = () => {
+   router.push('/product?name=mobile&price=200')
+}
   return (
     <div>
-      
+      <button onClick={goTo}>Click</button>
     </div>
   );
 };
