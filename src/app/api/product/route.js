ff3637a51a1ecc5e3b45d50ddfa9240793288783
hablu <131282586+hablu@users.noreply.github.com>
2024-02-22 19:34:29 +0600

    many api route create

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -1,5 +1,24 @@
 import { NextResponse } from "next/server"
 //get
 export const GET = async () => {
-  
+   return NextResponse.json();
 }
+
+//post
+export const POST = async (req, res) => {
+  return NextResponse.json();
+}
+// patch
+export const PATCH = async (req, res) => {
+  return NextResponse.json();
+};
+
+// put
+export const PUT = async (req, res) => {
+  return NextResponse.json();
+};
+
+// delete
+export const DELETE = async (req, res) => {
+  return NextResponse.json();
+};
