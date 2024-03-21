9a8a3030759bea8589095f55a5c02cbe830a7555
hablu <131282586+hablu@users.noreply.github.com>
2024-03-21 11:59:02 +0600

    createmany user

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -3,13 +3,25 @@ import { NextResponse } from "next/server"
 
 export const POST = async (req, res) => {
   const prisma = new PrismaClient()
-  const result = await prisma.users.create({
-    data: {
-      fName: "Rashed",
-      lName: "akash",
-      age:28
-    }
-  })
+  const result = await prisma.users.createMany({
+    data: [
+      {
+        fName: "Rashed",
+        lName: "akash",
+        age: 28,
+      },
+      {
+        fName: "usman",
+        lName: "akash",
+        age: 28,
+      },
+      {
+        fName: "umar",
+        lName: "akash",
+        age: 28,
+      },
+    ],
+  });
 
   return NextResponse.json(result)
 }
